73543022eb5a2f6cf363c634503a2a799d9e0981
Aleksandra <kuklina.aleksandra@rambler.ru>
2021-04-06 22:48:20 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[5, 5];
+
+            /*   for (int i =0; i<5; i++) 
+               {
+                   for (int j = 0; j < 5; j++)
+                   {
+                       if (i==j)
+                       {
+                           Console.Write(1);
+                       }
+                       else
+                       {
+                           Console.Write(0);
+                       }
+
+                   }
+                   Console.WriteLine();
+               }
+               Console.WriteLine();
+               Console.ReadKey();
+
+            */
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 4; j >=0; j--)
+                {
+                    if (i == j)
+                    {
+                        Console.Write(1);
+                    }
+                    else
+                    {
+                        Console.Write(0);
+                    }
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
